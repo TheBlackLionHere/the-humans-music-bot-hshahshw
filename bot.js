cb6dc486d5810461a294687d858cb845e5908dce
TheBlackLionHere <41819853+TheBlackLionHere@users.noreply.github.com>
2018-07-29 13:27:13 +0300

    Update bot.js

--- bot.js
+++ bot.js
@@ -1,14 +1,31 @@
-const Discord = require("discord.js");
+const Discord = require('discord.js');
 const client = new Discord.Client();
+const prefix = '.'
+
 client.on('ready', () => {
-  client.user.setGame(`Edting  | *help | By TheBlackLion_ .`,'https://www.twitch.tv/v5bz');
-  console.log('---------------');
+  console.log(`Logged in as ${client.user.tag}!`);
+client.user.setGame(`Nothing`,"http://twitch.tv/S-F")
+  console.log('')
+  console.log('')
+  console.log('╔[═════════════════════════════════════════════════════════════════]╗')
+  console.log(`[Start] ${new Date()}`);
+  console.log('╚[═════════════════════════════════════════════════════════════════]╝')
+  console.log('')
+  console.log('╔[════════════════════════════════════]╗');
+  console.log(`Logged in as * [ " ${client.user.username} " ]`);
+  console.log('')
+  console.log('Informations :')
+  console.log('')
+  console.log(`servers! [ " ${client.guilds.size} " ]`);
+  console.log(`Users! [ " ${client.users.size} " ]`);
+  console.log(`channels! [ " ${client.channels.size} " ]`);
+  console.log('╚[════════════════════════════════════]╝')
+  console.log('')
+  console.log('╔[════════════]╗')
   console.log(' Bot Is Online')
-  console.log('---------------')
+  console.log('╚[════════════]╝')
+  console.log('')
+  console.log('')
+});
 
-
-
-
-
- });
-client.login("");
+client.login(process.env.BOT_TOKEN);
