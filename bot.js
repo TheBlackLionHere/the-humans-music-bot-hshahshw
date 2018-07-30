1749b8e930cc5ea93be69536dfebb2b44dd2f0e8
TheBlackLionHere <41819853+TheBlackLionHere@users.noreply.github.com>
2018-07-30 16:34:53 +0300

    Update bot.js

--- bot.js
+++ bot.js
@@ -281,16 +281,7 @@ message.react("❌")
    message.channel.send({embed});
       }
   });
-client.on("guildMemberAdd", function(member) {
-    const wc = member.guild.channels.find("name", "member-log")
-        const embed = new Discord.RichEmbed()
-        .setColor('B90C0C')
-        .setAuthor(member.user.tag, member.user.avatarURL)
- .setDescription('***يا مرحبا وسهلاً بضيف لفانا، يزهي بك الأدب العربي وينثر لك أزهار يسقيك من نبع المشاعر وفانا، لين الهلا تثمر على غصونك أطيار. ***')
-.setThumbnail(member.avatarURL)
-  .setImage('https://www.askideas.com/media/13/Welcome-Deers-Sign.jpg')
-        .setTimestamp()
-        return wc.sendEmbed(embed);
+;
         
 });
 client.on('message', function(msg) {
@@ -311,4 +302,56 @@ client.on('message', function(msg) {
       msg.channel.send({embed:embed});
     }
   });
+const Discord = require("discord.js");
+const client = new Discord.Client();
+
+
+
+
+
+client.on('guildMemberAdd', member => {
+    let channel = member.guild.channels.find('name', 'welcome');
+    let memberavatar = member.user.avatarURL
+      if (!channel) return;
+    let embed = new Discord.RichEmbed()
+        .setColor('RANDOM')
+        .setThumbnail(memberavatar)
+        .addField(':running_shirt_with_sash: | name :  ',`${member}`)
+        .addField(':loudspeaker: | اهلا ومسهلا ولكم نورت السيرفر' , `اهلاً بك في السيرفر, ${member}`)
+        .addField(':		id: | user :', "**[" + `${member.id}` + "]**" )
+                .addField('➡| انت العضو رقم',`${member.guild.memberCount}`)
+               
+                  .addField("Name:",`<@` + `${member.id}` + `>`, true)
+                     
+                                     .addField(' الـسيرفر', `${member.guild.name}`,true)
+                                       
+     .setFooter(`${member.guild.name}`)
+        .setTimestamp()
+   
+      channel.sendEmbed(embed);
+    });
+    
+    client.on('guildMemberRemove', member => {
+        var embed = new Discord.RichEmbed()
+        .setAuthor(member.user.username, member.user.avatarURL)
+        .setThumbnail(member.user.avatarURL)
+        .setTitle(`لقد خرج صديق :( :raised_hand::skin-tone-1: :pensive:`)
+        .setDescription(`مع السلامة  :raised_hand::skin-tone-1: :pensive: `)
+        .addField(':bust_in_silhouette:   تبقي',`**[ ${member.guild.memberCount} ]**`,true)
+        .setColor('RED')
+        .setFooter(`====شكرا للاستمتاع====`, ' https://cdn.discordapp.com/attachments/397818254439219217/399292026782351381/shy.png')
+    
+    var channel =member.guild.channels.find('name', 'welcome')
+    if (!channel) return;
+    channel.send({embed : embed});
+    })
+	
+	
+	
+	
+	
+	
+	
+	
+client.login('');
 client.login(process.env.BOT_TOKEN)
