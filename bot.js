b76d7b558cfaeec381f2116335d59270c2e01a26
TheBlackLionHere <41819853+TheBlackLionHere@users.noreply.github.com>
2018-07-31 17:58:16 +0300

    Update bot.js

--- bot.js
+++ bot.js
@@ -21,7 +21,7 @@ client.on('message', msg => {
 });
 client.on('message', message => {
     if (message.author.bot) return;
-     if (message.content === prefix + "help-en") {
+     if (message.content === prefix + "help-eb") {
 		 message.channel.send('**The Message Was Sent On Private**');
             
 	
