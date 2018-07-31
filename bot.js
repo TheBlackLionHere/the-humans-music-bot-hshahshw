eaaf7ad9fa415773930d668810aeac9341f843d2
TheBlackLionHere <41819853+TheBlackLionHere@users.noreply.github.com>
2018-07-31 17:35:10 +0300

    Update bot.js

--- bot.js
+++ bot.js
@@ -19,31 +19,86 @@ client.on('message', msg => {
     msg.reply('Pong!');
   }
 });
-if (message.content === '$help') {
-              var embed  = new Discord.RichEmbed()
-                .addField("**LINKS**" ,":no_entry_sign: تم اضافه منع الروابط :no_entry_sign: ")
-                .addField("broadcast (bc)" ,"**الاستخدام:** ``$broadcast <الرساله> , $bc <الرساله>``")
-                .addField("**BAN**" ,"**الاستخدام:** ``$ban <المستخدم>``")
-                .addField("**KICK**" ,"**الاستخدام:** ``$kick <المستخدم> ``")
-                .addField("**ِAVATAR**" ,"**الاستخدام:** ``$avatar``")
-                .addField("**INFO**", "**الأستخدام :** ``$info``")
-                .addField("**SAY**" ,"**الاستخدام:** ``$say <الرساله>``")
-                .addField("**ID**" ,"**ال��ستخدام:** ``$id``")
-                .addField("**SERVER**" ,"**الاستخدام:** ``$server``")
-                .addField("**INVITE**" ,"**الاستخدام:** ``$invite <لأضافه البوت لأى سيرفر>``")
-                .addField("**SUPPORT**" ,"**الاستخدام:** ``$suppport <سيرفر دعم الفنى>``")
-                .addField("**QA**" ,"**الاستخدام:** ``$qa <السؤال>``  ")
-                .addField("**CLEAR**" ,"**الاستخدام:** ``$clear <العدد>``")
-                .addField("**PING**", "**الأستخدام:** ``$ping``")
-                .addField("**SERVERNAME**", "**الأستخدام:** ``$servername``")
-                .addField("**CUT_TWEET**", "**الاستخدام** ``$ct``")
-                .addField("**TWEET**", "**الاستخدام** ``$tweet <الرساله>``")
-                .addField("**NO INVITE LINKE**","تم اضافة خاصية منع الانفيتات ")
-                .addField("**LOGIN**" , " تم اضافة خاصية التفعيل لطلب تشغيلها في السيرفر كلم المبيرمجين ")
-                .setColor('RANDOM')
-.setColor('RANDOM')
-  message.author.sendEmbed(embed);
+client.on('message', message => {
+    if (message.author.bot) return;
+     if (message.content === prefix + "help-en") {
+		 message.channel.send('**The Message Was Sent On Private**');
+            
+	
+		 
+
+
+ message.author.sendMessage(`
+ **
+__~~TheHumans-Bot~~__ Got By: TheBlackLion_
+╱╭╮╭╮╱╱╱╱╭╮╭━╮╱╱╱╱╱╱╭━━╮╱╱╱╭╮
+╭╯╰┫┃╱╱╱╱┃┃┃╭╯╱╱╱╱╱╱┃╭╮┃╱╱╭╯╰╮
+╰╮╭┫╰━┳━━┫╰╯╯╭┳━╮╭━━┫╰╯╰┳━┻╮╭╯
+╱┃┃┃╭╮┃┃━┫╭╮┃┣┫╭╮┫╭╮┃╭━╮┃╭╮┃┃
+╱┃╰┫┃┃┃┃━┫┃┃╰┫┃┃┃┃╰╯┃╰━╯┃╰╯┃╰╮
+╱╰━┻╯╰┻━━┻╯╰━┻┻╯╰┻━╮┣━━━┻━━┻━╯
+╱╱╱╱╱╱╱╱╱╱╱╱╱╱╱╱╱╭━╯┃
+╱╱╱╱╱╱╱╱╱╱╱╱╱╱╱╱╱╰━━╯
+╔[❖════════════❖]╗
+             Prefix = ' $ '
+╚[❖════════════❖]╝
+
+╔[❖════════════❖]╗
+             Staff Commands
+╚[❖════════════❖]╝
+
+ ❖ $kick <mention > ➾ kick member from server
+
+ ❖ $clear ➾ clear chat 
+ 
+ ❖ $mute < mention > < mute time >  ➾ mute member
+
+ ❖ $unmute <mention> ➾ unmute member
+
+ ❖ $move <mention>  ➾ move member
+
+ ❖ $ban  <mention > band member from server
+  
+ ❖ $bc <message> ➾ message all members in server
+
+ ❖ $mutechannel  ➾ for lock the channel/ تقفيل الشات
+ 
+ ❖ $unmutechannel  ➾ unlock Channel/ الغاء تقفيل الشات
+ 
+ ❖ note :   طبعا الاوامر هاي مش  لكل الستاف في فرق بين  رتبة ورتبة ورح تفهم كل شي  لما تترقى وكمان ملاحظة الاكواد دي قابلة للتغير في اي وقت
+
+╔[❖════════════❖]╗
+            General & Funny Commands
+╚[❖════════════❖]╝
+
+❖ $image ➾ server avatar
+
+❖ $id ➾ your id
+
+❖ *avatar ➾ your avatar account
+
+❖ $ping ➾ to see ping
+
+❖ $زواج ➾  just for fun
+
+❖ $server ➾ server informations 
+
+❖ $invite ➾ bot invite link
+
+❖ $say  ➾  اكتب شي والبوت يقوله
+
+❖ note :    طبعا الاكواد  هاي للكل وممكن ينضاف عليها اي شي و بأي وقت وبس السلام عليكم
+
+╔[❖════════════❖]╗
+                    Welcome
+╚[❖════════════❖]╝
+
+
+
+`);
+
     }
+});
 });
 client.on('message', message => {
   if(message.content === ('clear')) {
